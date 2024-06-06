5405ca2d685f9ca4320614920b08dc4cdd2fb82f
Anirudh Kamath <github@kamath.io>
2024-06-05 18:07:26 -0700

    landing

---
+++ landing-page/app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="flex items-center justify-center h-screen p-8 overflow-x-hidden">
+			<div>
+				<div className="h-64 z-[-1] flex place-items-center bg-gradient-radial from-blue-700/80 via-blue-700/10 to-transparent w-full">
+					<div className="items-center justify-center flex items-center justify-center w-full">
+						<Image
+							className="drop-shadow-[0_0_0.1rem_#ffffff70]"
+							src="/img/barbell.svg"
+							alt="Next.js Logo"
+							width={500}
+							height={0}
+							priority
+						/>
+					</div>
+				</div>
+				<div className="w-full flex flex-col gap-4">
+					<h1 className="text-4xl md:text-8xl font-bold w-full left-0 text-center">Barbell cURLs</h1>
+					<h3 className="text-lg md:text-4xl w-full left-0 text-center">Coming Soon.</h3>
+				</div>
+			</div>
+		</main>
+	);
+}
