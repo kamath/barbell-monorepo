eeed274bd9ffc70e5571c2ee59c66580847d45ee
Anirudh Kamath <github@kamath.io>
2024-06-10 18:43:32 -0700

    add secrets from SM

--- bolt-bot/src/app.ts
+++ bolt-bot/src/app.ts
@@ -1,5 +1,15 @@
+import { AWS_REGION, AWS_SECRET_NAME } from './consts';
+import WebClient from './node_modules/@slack/web-api/dist/WebClient';
 import { SlackBot, Action, BlockActionHandler } from './sdk';
+import { getSecret } from './utils/aws';
 
-const bot = new SlackBot(process.env.SLACK_EVENT_SIGNING_SECRET || "", process.env.SLACK_TOKEN || "", Number(process.env.PORT) || 3000);
+const BotSecrets: {
+	SLACK_EVENT_SIGNING_SECRET: string;
+	SLACK_TOKEN: string;
+	PORT: number;
+} = JSON.parse(await getSecret(AWS_SECRET_NAME, AWS_REGION) || "{}");
 
-export { bot, Action, BlockActionHandler };
+const bot = new SlackBot(BotSecrets.SLACK_EVENT_SIGNING_SECRET || "", BotSecrets.SLACK_TOKEN || "", Number(BotSecrets.PORT) || 3000);
+const slackClient = new WebClient(BotSecrets.SLACK_TOKEN || "");
+
+export { bot, Action, BlockActionHandler, slackClient };
