16d06a13e1bda8a286fe8fe9cc042d20dda1e1f4
Anirudh Kamath <github@kamath.io>
2024-06-25 14:37:43 -0700

    remove solaris barbell

--- solaris-barbell/prisma/migrations/20240607050005_add_gate/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "GateLastOpened" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
-
-    CONSTRAINT "GateLastOpened_pkey" PRIMARY KEY ("id")
-);
