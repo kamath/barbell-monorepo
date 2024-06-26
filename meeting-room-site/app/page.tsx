39cfa6916cf9a06ba1decde354726128152463cc
Anirudh Kamath <github@kamath.io>
2024-06-25 18:18:59 -0700

    build error

--- meeting-room-site/app/page.tsx
+++ meeting-room-site/app/page.tsx
@@ -16,11 +16,10 @@ function formatHumanReadableDate(dateStr: string): string {
 	});
 }
 
-export default function Home() {
+function DisplayBooking() {
 	const searchParams = useSearchParams();
-	const [bookingData, setBookingData] = useState<Booking | null>(null);
-
 	const CONFERENCE_ROOM = searchParams.get('room') || 'Conference Room 1';
+	const [bookingData, setBookingData] = useState<Booking | null>(null);
 
 	useEffect(() => {
 		const fetchBookings = async () => {
@@ -36,36 +35,38 @@ export default function Home() {
 	// Start date is in the past and end date is in the future
 	if (bookingData && new Date(bookingData.startDate) < new Date() && new Date(bookingData.endDate) > new Date()) {
 		return (
-			<Suspense fallback={<div>Loading...</div>}>
-				<main className="flex h-screen items-center justify-center bg-red-100">
-					<div className="flex flex-col items-center justify-center">
-						<h1 className="text-4xl font-bold">{CONFERENCE_ROOM}</h1>
-						<p className="text-xl">Booked from {formatHumanReadableDate(bookingData.startDate)} to {formatHumanReadableDate(bookingData.endDate)}</p>
-					</div>
-				</main>
-			</Suspense>
+			<main className="flex h-screen items-center justify-center bg-red-100">
+				<div className="flex flex-col items-center justify-center">
+					<h1 className="text-4xl font-bold">{CONFERENCE_ROOM}</h1>
+					<p className="text-xl">Booked from {formatHumanReadableDate(bookingData.startDate)} to {formatHumanReadableDate(bookingData.endDate)}</p>
+				</div>
+			</main>
 		);
 	}
 
 	// Start date is in the future
 	if (bookingData && new Date(bookingData.startDate) > new Date()) {
 		return (
-			<Suspense fallback={<div>Loading...</div>}>
-				<main className="flex h-screen items-center justify-center bg-green-100">
-					<div className="flex flex-col items-center justify-center">
-						<h1 className="text-4xl font-bold">{CONFERENCE_ROOM}</h1>
-						<p className="text-xl">Next booking starts at {formatHumanReadableDate(bookingData.startDate)}</p>
-					</div>
-				</main>
-			</Suspense>
+			<main className="flex h-screen items-center justify-center bg-green-100">
+				<div className="flex flex-col items-center justify-center">
+					<h1 className="text-4xl font-bold">{CONFERENCE_ROOM}</h1>
+					<p className="text-xl">Next booking starts at {formatHumanReadableDate(bookingData.startDate)}</p>
+				</div>
+			</main>
 		);
 	}
-	return <Suspense fallback={<div>Loading...</div>}>
+	return (
 		<main className="flex h-screen items-center justify-center bg-green-100">
 			<div className="flex flex-col items-center justify-center">
 				<h1 className="text-4xl font-bold">{CONFERENCE_ROOM}</h1>
 				<p className="text-xl">No bookings!</p>
 			</div>
 		</main>
+	)
+}
+
+export default function Home() {
+	return <Suspense fallback={<div>Loading...</div>}>
+		<DisplayBooking />
 	</Suspense>;
 }
