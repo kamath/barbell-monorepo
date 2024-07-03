1c8328f7ad5b622c8f778a3aacc3f6c0ad7d2df1
Anirudh Kamath <github@kamath.io>
2024-07-03 12:18:56 -0700

    axe aws

--- barbell/src/barbell/consts.ts
+++ barbell/src/barbell/consts.ts
@@ -1,10 +1,8 @@
-import { getSecret } from "./utils/aws"
-
 type Environment = 'DEVELOPMENT' | 'PROD'
 export const INIT_MODAL_NAME = "Barbell cURL"
 export const INIT_ACTION_ID = "SOME_ACTION"
 
 export const ENVIRONMENT: Environment = process.env.ENVIRONMENT as Environment || "DEVELOPMENT"
-export const SLACK_SECRETS = await getSecret("slack_global_" + ENVIRONMENT, "us-east-1") as {
-	SLACK_OAUTH_TOKEN: string
+export const SLACK_SECRETS = {
+	SLACK_OAUTH_TOKEN: process.env.SLACK_OAUTH_TOKEN || ""
 }
--- barbell/src/consts.ts
+++ barbell/src/consts.ts
@@ -1,6 +1,8 @@
-import { getSecret } from "./utils/aws"
-
 export const ENVIRONMENT = process.env.ENVIRONMENT || "DEVELOPMENT"
 export const AWS_SECRET = `solaris_garage_${ENVIRONMENT}`
 
-export const { PARKING_CHANNEL_ID, JACOB_SLACK_ID, ALERT_CHANNEL_ID, MISSION_ST_SWITCH_URL, OTIS_ST_SWITCH_URL } = await getSecret(AWS_SECRET)
+export const PARKING_CHANNEL_ID = process.env.PARKING_CHANNEL_ID || "";
+export const JACOB_SLACK_ID = process.env.JACOB_SLACK_ID || "";
+export const ALERT_CHANNEL_ID = process.env.ALERT_CHANNEL_ID || "";
+export const MISSION_ST_SWITCH_URL = process.env.MISSION_ST_SWITCH_URL || "";
+export const OTIS_ST_SWITCH_URL = process.env.OTIS_ST_SWITCH_URL || "";
