5b98aa54e025a31efd49ea80434b7784033c42c9
Anirudh Kamath <github@kamath.io>
2025-12-16 16:38:06 -0500

    refactor slack utility functions for improved readability and consistency

--- barbell/src/barbell/consts.ts
+++ barbell/src/barbell/consts.ts
@@ -1,8 +1,8 @@
-type Environment = 'DEVELOPMENT' | 'PROD'
-export const INIT_MODAL_NAME = "Barbell cURL"
-export const INIT_ACTION_ID = "SOME_ACTION"
-
-export const ENVIRONMENT: Environment = process.env.ENVIRONMENT as Environment || "DEVELOPMENT"
+type Environment = "DEVELOPMENT" | "PROD";
+export const INIT_MODAL_NAME = "Barbell cURL";
+export const INIT_ACTION_ID = "SOME_ACTION";
+export const ENVIRONMENT: Environment =
+  (process.env.ENVIRONMENT as Environment) || "DEVELOPMENT";
 export const SLACK_SECRETS = {
-	SLACK_OAUTH_TOKEN: process.env.SLACK_OAUTH_TOKEN || ""
-}
+  SLACK_OAUTH_TOKEN: process.env.SLACK_OAUTH_TOKEN || "",
+};
